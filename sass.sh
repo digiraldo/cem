d7b898fbd916d22367c58ba394bcb33f0aad7624
digiraldo <disaned1@gmail.com>
2022-06-13 22:19:40 -0500

    Instalando dependencias

--- sass.sh
+++ sass.sh
@@ -23,6 +23,15 @@ npm run sass
 npm i -D gulp
 npm install gulp --save-dev
 
+# Creamos gulpfile.js en la raiz del proyecto
+# Este va a ser el contenedor de nuestras tarea
+# Se crea el script tarea y se llama
+#* npm run tarea
+#* npx gulp tarea
+
+# para que gulp se conecte con sass
+npm i -D gulp-sass
+
 
 # =================================================================================
-npm install sass gulp --save-dev
+npm install sass gulp gulp-sass postcss gulp-webp gulp-sourcemaps gulp-postcss gulp-plumber gulp-imagemin@7.1.0 gulp-cache gulp-avif cssnano autoprefixer --save-dev
