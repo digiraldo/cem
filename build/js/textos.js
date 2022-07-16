ba109c1623de66ec275b56ec37dcfa3f1aa6bc1d
digiraldo <disaned@hotmail.com>
2022-07-15 23:12:12 -0500

    Configuracion para telefonos del hero

--- build/js/textos.js
+++
@@ -1,7 +0,0 @@
-fitText(document.querySelector("p"), 0.38);
-
-
-window.addEventListener("scroll", function () {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0)
-})
